fdf04128c4a90b8fd3701038b3aebf74ceac259c
ExraaG <alexhaidulprafa@gmail.com>
2026-05-03 11:01:43 +0200

    Set release version to 1.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 5
-        versionName = "1.4.0"
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
